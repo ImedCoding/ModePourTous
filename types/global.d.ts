5d595f6947000d022d5f68b54404594de61dcd1d
ImedItescia <imededdinefodeilla@gmail.com>
2026-02-12 13:05:18 +0100

    Refactor global declaration for JSX intrinsic elements to improve type definitions for "dotlottie-player"

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,16 +1,20 @@
 import type React from "react";
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "dotlottie-player": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement>,
-      HTMLElement
-    > & {
-      src?: string;
-      background?: string;
-      speed?: string | number;
-      loop?: boolean;
-      autoplay?: boolean;
-    };
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "dotlottie-player": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      > & {
+        src?: string;
+        background?: string;
+        speed?: string | number;
+        loop?: boolean;
+        autoplay?: boolean;
+      };
+    }
   }
 }
+
+export {};
